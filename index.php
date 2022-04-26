11cc050eefeeed48b324e701ebd2bdb019647faf
moctar-diop <diopmoctar92i@gmail.com>
2022-04-26 13:36:24 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/css/bootstrap.css">
+    <script src="css/js/bootstrap.js"></script>
+</head>
+<body>
+    <header>
+      
+     
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="image/logo.jpeg" alt="..." height="86">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr class="dropdown-divider">
+              </li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</header>
+
+<div class="container bg-light">
+  
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <h1 class="xlarge-font"><b>GMS</b></h1>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Pourquoi nous?</b></h1>
+      <!-- Decription du cabinet et objetif- vision-  -->
+      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button class="btn btn-secondary">Download Application</button>
+    </div>
+    <div class="col">
+        <img src="image/logo.jpeg" width="335" height="471">
+    </div>
+  </div>
+</div>
+
+
+<div class="container ">
+  <div class="row">
+  <div class="col">
+        <img src="image/bourse.jpg" width="335" height="471">
+    </div>
+    <div class="col-6">
+      <h1 class="xlarge-font"><b>Bourse de formation</b></h1>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Pourquoi nous?</b></h1>
+      <!-- Decription du cabinet et objetif- vision-  -->
+      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button class="btn btn-secondary">Download Application</button>
+    </div>
+    
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <h1 class="xlarge-font"><b>Ecole partenaire</b></h1>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Pourquoi nous?</b></h1>
+      <!-- Decription du cabinet et objetif- vision-  -->
+      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button class="btn btn-secondary">Download Application</button>
+    </div>
+    <div class="col">
+        <img src="image/logo.jpeg" width="335" height="471">
+    </div>
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+  <div class="col">
+        <img src="image/logement.jpg" width="100%" height="471">
+    </div>
+    
+    <div class="col-6">
+      <h1 class="xlarge-font"><b>Logement</b></h1>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Pourquoi nous?</b></h1>
+      <!-- Decription du cabinet et objetif- vision-  -->
+      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button class="btn btn-secondary">Download Application</button>
+    </div>
+   
+  </div>
+</div>
+
+
+<div class="container">
+  <div class="row">
+    <div class="col-6">
+      <h1 class="xlarge-font"><b>Transport</b></h1>
+      <h1 class="large-font" style="color:MediumSeaGreen;"><b>Pourquoi nous?</b></h1>
+      <!-- Decription du cabinet et objetif- vision-  -->
+      <p><span style="font-size:36px">Take photos like a pro.</span> You should buy this app because lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <button class="btn btn-secondary">Download Application</button>
+    </div>
+    <div class="col">
+        <img src="image/bus.png" width="335" height="471">
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+
+</body>
+</html>
